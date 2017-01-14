a06cebfe491a937226f9b205182e571b5574b063
najibu <ns.najibu@gmail.com>
2017-01-14 22:10:27 +0000

    Added pages controller and actions

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.master')
+@section('title', 'About')
+
+@section('content')
+    <div class="Jumbotron">
+        <h1 class="title">
+            About Page
+        </h1>
+
+        <p class="quote">
+            Our about page.
+        </p>
+    </div>
+    </div>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.master')
+@section('title', 'Contact')
+
+@section('content')
+    <div class="Jumbotron">
+        <h1 class="title">
+            Contact Page
+        </h1>
+
+        <p class="quote">
+            Our contact page.
+        </p>
+    </div>
+    </div>
+@endsection
